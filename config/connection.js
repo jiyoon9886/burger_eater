c44f4472c2bd29b210989abf2fdd5416a9f6710e
Jiyoon Kim <jiyoon9886@gmail.com>
2020-05-08 11:15:33 -0400

    updated sql password

--- config/connection.js
+++ config/connection.js
@@ -4,7 +4,7 @@ var connection = mysql.createConnection({
   host: "localhost",
   port: 3306,
   user: "root",
-  password: "jy2052927",
+  password: process.env.DB_PASSWORD,
   database: "burgers_db"
 });
 
